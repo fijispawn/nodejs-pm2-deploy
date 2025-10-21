10ba4fcf53ab7211f4a1029a9d9adeec0926eb5f
Mikhail Smirnov <m2004029@edu.misis.ru>
2025-10-21 11:44:27 +0300

    корс волнерабилитис

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,23 +4,44 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+import cors from 'cors';
+
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, CLIENT_ORIGIN } = process.env;
+
 const app = express();
+
 mongoose.connect(DB_ADDRESS);
 
-// Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
+const allowedOrigin = CLIENT_ORIGIN || 'https://mestechko.nomorepartiessbs.ru';
+
+app.use(
+  cors({
+    origin: allowedOrigin,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false,
+  }),
+);
+
+app.options(
+  '*',
+  cors({
+    origin: allowedOrigin,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(routes);
 app.use(errors());
 app.use(errorHandler);
 
-// eslint-disable-next-line no-console
+
 app.listen(PORT, () => console.log('ok'));
